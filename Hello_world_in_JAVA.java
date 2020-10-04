79e48b163c1a6fc1b3533e554e69769f964225df
Swayamshree <mohantyswayamshree@gmail.com>
2020-10-04 23:50:56 +0530

    Print Hello World in JAVA

--- Hello_world_in_JAVA.java
+++ Hello_world_in_JAVA.java
@@ -1,7 +1,13 @@
 public class Hello_world_in_JAVA
-{
+//This is the default Class, It's name same with it's file name
+
+{ 
+    //This is the default function, i.e main()                            
     public static void main(String[] args)
     {
+
+        //Here you have to write the sentence, which you want to display as output
         System.out.println("Hello World");
+        
     }
 }
